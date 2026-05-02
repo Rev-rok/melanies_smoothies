017d5b1b6a8d83d7c167dbc03b62de24e952c127
Sten <112997109+Rev-rok@users.noreply.github.com>
2026-05-01 22:30:46 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -24,7 +24,7 @@ ingredient_list = st.multiselect(
 )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 if ingredient_list:
     ingredients_string = ''
