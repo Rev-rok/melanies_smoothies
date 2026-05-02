b20ac9ec93a307863cec58531d4b96139c840471
Sten <112997109+Rev-rok@users.noreply.github.com>
2026-05-01 22:28:52 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests  
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customer Your Smoothie! :cup_with_straw:")
@@ -21,6 +22,9 @@ ingredient_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 if ingredient_list:
     ingredients_string = ''
